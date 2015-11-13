994b8ca480e00b894f91f795a6bc99a7c398d025
joelcoxokc <joel.cox.dev@gmail.com>
2015-11-13 14:09:47 -0800

    fix(disappearing-navbar): Fix issue when navigating to functions or vars,the navbar was removed

--- src/api/product.js
+++ src/api/product.js
@@ -2,6 +2,14 @@ import {bindable, inject} from 'aurelia-framework';
 import {Redirect} from 'aurelia-router';
 import {Router} from 'aurelia-router';
 import {Server} from 'backend/server';
+import {DOM} from 'aurelia-pal';
+
+
+function scrollToElement(element) {
+  let scrollPosition = element.offsetTop + element.offsetParent.offsetTop;
+  let scrollContainer = DOM.getElementById("page-host");
+      scrollContainer.scrollTop = scrollPosition;
+}
 
 @inject(Server, Router)
 export class Repository {
@@ -23,7 +31,7 @@ export class Repository {
           this.product = product;
           this.selectedProductVersion = productVersion;
           this.selectedVersion = productVersion.version;
-
+          console.log(this.selectedProductVersion);
           if (this.selectedProductVersion) {
             this.isDeps       = this.selectedProductVersion.dependencies && this.selectedProductVersion.dependencies.length;
             this.isClasses    = this.selectedProductVersion.classes && this.selectedProductVersion.classes.length > 0;
@@ -40,4 +48,139 @@ export class Repository {
       this.router.navigate(`#/${this.product.userName}/${this.product.productName}/${newValue}/doc/api/overview`);
     }
   }
+
+  scrollTo(type, name) {
+    let element = DOM.getElementById(`${type}-${name}`);
+    scrollToElement(element);
+  }
 }
+
+
+// export class BaseAPI {
+//   static parent = undefined;
+//   static opts   = undefined;
+//   constructor(opts) {
+//     BaseAPI.ops = BaseAPI.opts || opts;
+//     BaseAPI.parent = BaseAPI.parent || this;
+//     Object.assign(this, BaseAPI.ops);
+//   }
+//   init(name) {
+//     BaseAPI.parent[new.target.name]
+//   }
+//   get(){}
+//   post(){}
+//   update(){}
+//   remove(){}
+// }
+
+
+// export class Users extends BaseAPI {
+//   constructor() {
+//     super();
+//   }
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function get() {}
+// function post() {}
+// function update() {}
+// function remove() {}
+
+// export class PrivateAPI {
+//   get() {}
+//   post() {}
+//   update() {}
+//   remove() {}
+// }
+
+// export class API {
+//   constructor(nameOrUrl, parent) {
+//     if (parent) {
+//       this.name =  nameOrUrl;
+//       this.url  = parent.url;
+//     } else {
+//       this.url = url;
+//     }
+//   }
+
+//   createChild(name) {
+//     this[name] = new API(name, this);
+//   }
+
+//   get() { return get(this.url) }
+//   post(...args) { return post(this.url, ...args) }
+//   update(...args) { return update(this.url, ...args) }
+//   delete(...args) { return remove(this.url, ...args)}
+// }
+
+
+// var api = new API('myhost.com');
+
+// api.get();
+// api.createChild('users', 'myhost.com/users');
+
+// api.users.get();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class BaseModel {
+
+//   constuctor(opts, parent) {
+//     this.url = opts.url;
+//       this.token = opts.token;
+//   }
+
+//   request(url, method, form, statusCode) {
+//   }
+
+//   get(url) {
+//     return this.request(url, 'GET');
+//   }
+
+//   post(url, params) {
+//     return this.request(url, 'POST', params);
+//   }
+
+//   put(url, params) {
+//     return this.request(url, 'PUT', params);
+//   }
+
+//   delete(url, params) {
+//     return this.request(url, 'DELETE', params);
+//   }
+// }
+
+// export class GitLabUsers
+
+
+
+
