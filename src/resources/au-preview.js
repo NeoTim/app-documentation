f4275ab5f537a233fda91eb043acaa7f76d7c1ef
joelcoxokc <joel.cox.dev@gmail.com>
2015-11-17 23:44:17 -0800

    fix(remove-articles): Remove article-wraps

--- src/resources/au-preview.js
+++ src/resources/au-preview.js
@@ -1,7 +1,19 @@
-import {customElement, bindable} from 'aurelia-framework';
+import {customElement, bindable, inject} from 'aurelia-framework';
+
 
 @customElement('au-preview')
+@inject(Element)
 export class PreviewElement {
   @bindable view = null;
   @bindable viewModel = null;
+  titles = [];
+
+  constructor(element) {
+    this.element = element;
+  }
+
+  attached() {
+    let sections = this.element.querySelectorAll('narrative');
+    console.log(sections);
+  }
 }
