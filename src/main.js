e86560d72d166fc8aefb705b9ebc3b6c36e794a1
joelcoxokc <joel.cox.dev@gmail.com>
2015-11-19 11:14:56 -0800

    fix(Desktop-scroll): Add the scrollbar back on the desktop

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import {isAndroid} from 'aurelia-interface-platforms';
 
 export function configure(aurelia) {
   aurelia.use
@@ -6,6 +7,10 @@ export function configure(aurelia) {
     .plugin('aurelia-animator-css')
     .plugin('aurelia-interface-platforms', config => {
       config.setClassList(document.documentElement);
+      if (isAndroid) {
+        let meta = document.getElementById('metatag');
+        meta.setAttribute('content', 'width=device-width, initial-scale=1.0, user-scalable=no');
+      }
     })
     .feature('article/language')
     .globalResources('resources/au-icon', 'resources/au-preview');
